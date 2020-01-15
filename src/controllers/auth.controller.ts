e6f691f68684c0ab553d2f53242b30862d560edb
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-01-15 14:18:17 -0300

    spaces_signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,9 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 
 @Controller('api/auth/')
 export class AuthController {
+
 	@Post('signin')
 	async signin(request: Request, response: Response) {
 		const data = request.body;
 		return response.status(200).json(data);
 	}
+	
 }
